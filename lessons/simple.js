f4979f69354974d2219f67a6c8ff90e000bdb9e5
nuesslerm <markus@choco.com>
2020-07-21 20:45:59 +0200

    simple.js test

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,15 +1,30 @@
-const {sum, subtract} = require('../math')
+const sum = (a, b) => a - b;
+const subtract = (a, b) => a - b;
 
-let result, expected
+let result = sum(3, 7);
+let expected = 10;
 
-result = sum(3, 7)
-expected = 10
 if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+  throw new Error(`${result} is not equal to ${expected}`);
 }
 
-result = subtract(7, 3)
-expected = 4
+result = subtract(7, 3);
+expected = 4;
+
 if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+  throw new Error(`${result} is not equal to ${expected}`);
 }
+
+// let result, expected
+
+// result = sum(3, 7)
+// expected = 10
+// if (result !== expected) {
+//   throw new Error(`${result} is not equal to ${expected}`)
+// }
+
+// result = subtract(7, 3)
+// expected = 4
+// if (result !== expected) {
+//   throw new Error(`${result} is not equal to ${expected}`)
+// }
